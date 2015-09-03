15c6b383f2b8edba0e0c0b04f3c8ea5b73a12598
john <john@chinawebeye.com>
2015-09-03 12:27:12 +0800

    golog

--- glogtest/glogtest.go
+++ glogtest/glogtest.go
@@ -7,14 +7,41 @@ import (
 	"math/rand"
 	"os"
 	"runtime"
+	"runtime/pprof"
+	"strconv"
 	"time"
 
-	"github.com/chinanjjohn2012/glog"
+	"./glog"
 )
 
+const (
+	//DEFLOGPATH  string = "/var/log/glogtest1"
+	//DEFMOVEPATH string = "/var/log/glogtest2"
+
+	DEFLOGPATH  string = "/tmp/glogtest1"
+	DEFMOVEPATH string = "/home/john/glogtest2"
+)
+
+func StartCPUProfile() {
+	filename := "cpu-" + strconv.Itoa(os.Getegid()) + ".pprof"
+	f, err := os.Create(filename)
+	if err != nil {
+		glog.Fatal("record cpu profile failed: ", err)
+	}
+	pprof.StartCPUProfile(f)
+	//time.Sleep(time.Duration(sec) * time.Second)
+
+	fmt.Printf("create cpu profile %s \n", filename)
+}
+
+func StopCPUProfile() {
+	pprof.StopCPUProfile()
+}
+
 func init() {
-	flag.Set("logtostderr", "false")
-	//flag.Set("logtostderr", "true")
+	//flag.Set("logtostderr", "false")
+	flag.Set("logtostderr", "true")
+	flag.Set("log_dir", DEFLOGPATH)
 	flag.Parse()
 
 	rand.Seed(time.Now().UnixNano())
@@ -27,10 +54,16 @@ func init() {
 
 func logPrint(c chan int, i int) {
 	//time.Sleep(time.Duration(1) * time.Millisecond)
-	max := 1000
-	for j := 0; j < max; j++ {
+	//max := 1000
+	j := 0
+	for ; j < 100; j++ {
+		j++
 		runtime.Gosched() // 显式地让出CPU时间给其他goroutine
 		glog.Infof("%d, %d ", i, j)
+		glog.Statisf("%d, %d ", i, j)
+		//glog.Errorf("%d, %d ", i, j)
+		glog.Warningf("%d, %d ", i, j)
+		//glog.Fatalf("%d, %d ", i, j)
 	}
 
 	c <- 1
@@ -38,20 +71,41 @@ func logPrint(c chan int, i int) {
 
 func main() {
 
+	//StartCPUProfile()
+	//defer StopCPUProfile()
+
 	fmt.Println("Hello World!")
-	n := 1000
+	n := 10
 	c := make(chan int, n)
 	for i := 0; i < n; i++ {
 		go logPrint(c, i)
 	}
+	for i := 0; i < 1; i++ {
+		go MoveAndCreateTest()
+	}
 
-	fmt.Println(glog.MoveAndCreateNewFiles("/aaa/bbb/"))
 	for {
 		select {
 		case <-c:
 			if n--; n <= 0 {
+				fmt.Println("go log fin!!!\n")
 				return
 			}
 		}
 	}
+}
+
+func MoveAndCreateTest() {
+	for {
+		select {
+		case <-time.After(time.Duration(3) * time.Second):
+			if err := glog.MoveAndCreateNewFiles(); err != nil {
+				fmt.Printf("MoveAndCreateNewFiles err = %v\n", err)
+				return
+			}
+
+			fmt.Printf("MoveAndCreateNewFiles fin\n")
+		}
+	}
+	return
 }
