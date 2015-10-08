81533b962fd326b96ebcd3efc1796323733ef814
john <john@chinawebeye.com>
2015-10-08 11:51:22 +0800

    test 1

--- gintest/gintest.go
+++ gintest/gintest.go
@@ -2,13 +2,59 @@
 package main
 
 import (
+	"bufio"
 	"log"
+	"net"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+type (
+	responW interface {
+		http.ResponseWriter
+		http.Hijacker
+	}
+	responseWriter struct {
+		http.ResponseWriter
+		size   int
+		status int
+	}
+	responseWriterA struct {
+		http.ResponseWriter
+		numA    int
+		sizeA   int
+		statusA int
+	}
+)
+
+var _ responW = &responseWriter{}
+var _ responW = &responseWriterA{}
+
+func (w *responseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	log.Printf("responseWriter Hijack")
+	if w.size < 0 {
+		w.size = 0
+	}
+
+	return w.ResponseWriter.(http.Hijacker).Hijack()
+}
+
+func (w *responseWriterA) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	log.Printf("responseWriterA Hijack")
+	if w.sizeA < 0 {
+		w.sizeA = 0
+	}
+	hjack, ok := w.ResponseWriter.(http.Hijacker)
+	if ok {
+		return hjack.Hijack()
+	}
+	return w.ResponseWriter.(http.Hijacker).Hijack()
+}
+
 func main() {
+	//var Writer responW
+	//Writer.Hijack()
 
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
